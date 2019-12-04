66a270f75b9a812d1848f02189946ba03e80c356
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-04 23:16:55 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Application
+
+ def call(env)
+    resp = Rack::Response.new
+    
+    time = Time.now
+    hour = time.strftime("%k").to_i   # 0 - 23        
+    binding.pry
+    
+    if (0...12).include?(hour)
+      resp.write "Good Morning!\n"
+    elsif (12..23).include?(hour)
+      resp.write "Good Afternoon!\n"
+    end
+ 
+    resp.finish
+  end
+end
